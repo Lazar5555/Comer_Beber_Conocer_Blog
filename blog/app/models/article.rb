2b1c5ad355bfac6454037676acffacb5c7827dfe
Eleazar <poncebe@gmail.com>
2017-01-02 17:48:46 -0600

    Se agregaron campos a la tabla de articulos para que puedan almacenar una imagen. Se agrego :cover como parametro fuerte. Validacion para que solo acepte formatos validos de imagen en el modelo.

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -10,6 +10,9 @@ class Article < ApplicationRecord
 
     before_create :set_visits_count
 
+    has_attached_file :cover, styles: { medium: "1280x720", thumb: "800x600" }
+    validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+
     def update_visits_count
         self.update(visits_count: self.visits_count + 1)
     end
